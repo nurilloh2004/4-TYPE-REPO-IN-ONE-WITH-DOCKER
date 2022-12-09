074a7bbb6ac85db0a68f81c3019a3de74337d70a
nurilloh2004 <nurillohpython@gmail.com>
2022-12-09 13:54:08 +0500

    view

--- apps/app2/mysite/blog/views.py
+++ apps/app2/mysite/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+
+def post_list(request, id):
+    """Function for posting list."""
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post Found.')
+
+    return render(request, 'blog/post/list.html', {'post': post})
