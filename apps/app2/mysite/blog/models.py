5f4d3dc156b074e7cfd06f98ff46e7ca4c007f85
nurilloh2004 <nurillohpython@gmail.com>
2022-12-12 09:57:21 +0500

    Update models.py

--- apps/app2/mysite/blog/models.py
+++ apps/app2/mysite/blog/models.py
@@ -4,14 +4,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset()\
-                    .filter(status=Post.Status.PUBLISHED)
-
-
-
 class Post(models.Model):
     """Post model."""
     class Status(models.TextChoices):
@@ -21,8 +13,8 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+                    on_delete=models.CASCADE,
+                    related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -30,8 +22,6 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
-    objects = models.Manager()
-    published = PublishedManager()
 
 
     class Meta:
