78e111c45bc6b050550d9a6f2336e2561f46a34a
nurilloh2004 <nurillohpython@gmail.com>
2022-12-15 16:45:23 +0500

    all changed

---
+++ apps/app3/myshop/shop/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name']),]
+        verbose_name = 'category'
+        verbose_name = 'categories'
+
